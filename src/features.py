92d8cdf58976d1f8a1e9ace48a0bbd8c649c91f8
kjcha <kjchampoux@gmail.com>
2026-06-15 11:51:52 -0400

    - Finished the model choices/definitions - Started the training setup

---
+++ src/features.py
@@ -0,0 +1,6 @@
+
+#This section lists all the features and target variable that will be used for training the machine learning models. 
+#The features are based on the AQHI formula, which includes the concentrations of 3 pollutants (O3, NO2, PM2.5)
+
+TARGET_VARIABLE = "AQHI" #The target variable we want to predict is the AQHI index.
+
--- src/models.py
+++ src/models.py
@@ -1,19 +1,46 @@
 from skilearn.linear_model import LinearRegression
 from skilearn.metrics import mean_squared_error
 
+#The purpose of this file is just to define the machine learning models that will be used for training and prediction. 
+#The models will then be called in train.py to train the model on the training dataset and make predictions on the test dataset.
+
 #Sources used:
 #https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html
 #https://scikit-learn.org/stable/modules/generated/sklearn.metrics.mean_squared_error.html
 #https://scikit-learn.org/1.5/auto_examples/linear_model/plot_ols.html
+#https://www.geeksforgeeks.org/machine-learning/k-nearest-neighbors-knn-regression-with-scikit-learn/
+#https://www.geeksforgeeks.org/machine-learning/k-nearest-neighbours/
+#https://www.geeksforgeeks.org/machine-learning/knn-vs-decision-tree-in-machine-learning/
 
 
 
-#Linear Regression model (baseline model)
+#1) Linear Regression model (baseline model)
+#Reason: A good simple model to start with, since we are trying to predict a continuous variable (AQHI).
 def linear_regression_model():
-    #Creating a linear regression model object
-    LinReg = linear.model.LinearRegression()
-    #Training the model on the training dataset
-    LinReg.fit(X_train, y_train) **To be replaced with the actual training dataset variables**
+    return LinearRegression()
 
 
-    return LinReg
+#2) Lasso Regression model (regularization model)
+#Reason: Since we are including extra features outside the AQHI formula, we want to use a regularization model to prevent overfitting and improve the generalization of the model.
+def lasso_regression_model():
+    return Lasso(alpha=0.1) #Setting alpha to 0.1 for regularization strength, this can be tuned based on the dataset and performance.
+
+
+#3) K-nearest neighbors (KNN) regression model (non-linear model)
+#Reason: Does not assume anything about the data distribution. It can capture non-linear patterns of the features and the target variable (AQHI).
+#Important note: Scaling is very important here, since KNN uses the distance between data points to make predictions. If the features are not scalled properly, we can have bad results. 
+def knn_regression_model():
+    return KNeighborsRegressor(n_neighbors=5) #Setting n_neighbors to 5, this can be tuned based on the dataset and performance.
+
+
+#4) Decision Tree regression model (non-linear model)
+#Reason: Can be used to provide more information about feature important, and captures the data patterns, not the data points.
+def decision_tree_regression_model():
+    return DecisionTreeRegressor(max_depth=5) #Setting max_depth to 5 to prevent overfitting, this can be tuned based on the dataset and performance.
+
+
+#5) Random Forest regression model (ensemble model)
+#Reason: By using an emsemble of decision trees, we can improve the performance and reduce the overfitting of the model. It also makes the trees less correlated with each other, which imroves generalization.
+def random_forest_regression_model():
+    return RandomForestRegressor(n_estimators=100, max_depth=5) #Setting n_estimators to 100 for the number of trees in the forest, and max_depth to 5 to prevent overfitting, these can be tuned based on the dataset and performance.
+
